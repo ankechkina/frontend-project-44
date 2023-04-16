1ec25d174b8660357dc3786eeabcb2c30c02604e
Анастасия Кечкина <kechnastya@gmail.com>
2023-04-16 20:31:21 +0300

    discard previous commit changes

--- src/games/progression.js
+++ src/games/progression.js
@@ -5,24 +5,20 @@ const rules = 'What number is missing in the progression?';
 const getNumbersEnd = (numbersStart, numbersAmount, interval) => {
   let i = 0;
   let numbersEnd = numbersStart;
-
   while (i < numbersAmount) {
     numbersEnd += interval;
     i += 1;
   }
   return numbersEnd;
 };
-
 const getStringOfNumbers = () => {
   const numbersAmount = getRandomInt(5, 10);
   const numbersStart = getRandomInt(1, 50);
   const interval = getRandomInt(2, 10);
   const numbersEnd = getNumbersEnd(numbersStart, numbersAmount, interval);
   const removedNumberPlace = getRandomInt(1, numbersAmount);
-
   let i = numbersStart;
   let wholeStringOfNumbers = '';
-
   while (i <= numbersEnd) {
     wholeStringOfNumbers = `${wholeStringOfNumbers} ${i}`;
     i += interval;
@@ -30,29 +26,25 @@ const getStringOfNumbers = () => {
   wholeStringOfNumbers = wholeStringOfNumbers.trim();
   return [wholeStringOfNumbers, removedNumberPlace];
 };
-
 const getRemovedNumberArray = () => {
   const [wholeStringOfNumbers, removedNumberPlace] = getStringOfNumbers();
   const separator = ' ';
   const separatedNumbers = wholeStringOfNumbers.split(separator);
   const removedNumber = separatedNumbers[removedNumberPlace - 1];
-
   const removedNumberArray = [];
-  for (let i = 0; i < separatedNumbers.length; i += 1) {
-    if (separatedNumbers[i] === removedNumber) {
+  for (const number of separatedNumbers) {
+    if (number === removedNumber) {
       removedNumberArray.push('..');
     } else {
-      removedNumberArray.push(separatedNumbers[i]);
+      removedNumberArray.push(number);
     }
   }
   return [removedNumberArray, removedNumber];
 };
-
 const generateRound = () => {
   const [removedNumberArray, removedNumber] = getRemovedNumberArray();
   const userQuestion = removedNumberArray.join(' ');
   const correctAnswer = removedNumber;
   return [userQuestion, correctAnswer];
 };
-
 export { rules, generateRound };
--- src/index.js
+++ src/index.js
@@ -1,28 +1,23 @@
 import readlineSync from 'readline-sync';
 
 export const getRandomInt = (min, max) => {
-  let minNumber = min;
-  let maxNumber = max;
-  minNumber = Math.ceil(minNumber);
-  maxNumber = Math.floor(maxNumber);
-  return Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber;
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 };
-
 const roundsCount = 3;
-
 export const startGame = (rules, generateRound) => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
   console.log(rules);
-
   for (let i = 0; i < roundsCount; i += 1) {
     const [userQuestion, correctAnswer] = generateRound();
     console.log(`Question: ${userQuestion}`);
     const userAnswer = readlineSync.question('Your answer: ');
-    if (userAnswer === correctAnswer && i === 2) {
+    if (userAnswer == correctAnswer && i === 2) {
       console.log(`Congratulations, ${userName}!`);
-    } else if (userAnswer === correctAnswer && i < 2) {
+    } else if (userAnswer == correctAnswer && i < 2) {
       console.log('Correct!');
     } else if (userAnswer !== correctAnswer) {
       console.log(
