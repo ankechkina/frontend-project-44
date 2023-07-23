8a4da96a8f84d063ca7e1ff0fd1a7ecf7a089916
Анастасия Кечкина <kechnastya@gmail.com>
2023-07-23 17:12:46 +0300

    simplify round counting

--- src/index.js
+++ src/index.js
@@ -1,28 +1,24 @@
 import readlineSync from 'readline-sync';
 
-const roundsCount = 3;
-
 const startGame = (rules, generateRound) => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
   console.log(rules);
-  for (let i = 0; i < roundsCount; i += 1) {
+  for (let i = 0; i < 3; i += 1) {
     const [userQuestion, correctAnswer] = generateRound();
     console.log(`Question: ${userQuestion}`);
     const userAnswer = readlineSync.question('Your answer: ');
-    if (userAnswer === correctAnswer && i === 2) {
-      console.log(`Congratulations, ${userName}!`);
-    } else if (userAnswer === correctAnswer && i < 2) {
+    if (userAnswer === correctAnswer) {
       console.log('Correct!');
     } else if (userAnswer !== correctAnswer) {
       console.log(
         `'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'`,
       );
       console.log(`Let's try again, ${userName}!`);
-      break;
+      return;
     }
-  }
+  } console.log(`Congratulations, ${userName}!`);
 };
 
 export default startGame;
